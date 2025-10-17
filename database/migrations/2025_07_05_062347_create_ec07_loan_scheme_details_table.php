31fc71e28fc629880a319e0c711262498f8c520d
Hari Narayan Das Pihu16 <hndas2016@gmail.com>
2025-10-17 09:19:59 +0530

    Desk: updated

--- database/migrations/2025_07_05_062347_create_ec07_loan_scheme_details_table.php
+++ database/migrations/2025_07_05_062347_create_ec07_loan_scheme_details_table.php
@@ -21,18 +21,21 @@ class CreateEc07LoanSchemeDetailsTable extends Migration
             $table->integer('loan_scheme_feature_standard_id')->nullable();
             $table->string('loan_scheme_feature_value')->nullable();
 
+            $table->boolean('is_featured')->default(false);
+            $table->boolean('is_open')->default(false);         // open to member
+            $table->boolean('is_calculated')->default(false);   
+            $table->boolean('is_regular')->default(false);      // for all emi
 
-            $table->double('min_amount',10,2)->nullable();
-            $table->double('max_amount',10,2)->nullable();
 
-            $table->integer('terms_in_month')->nullable();
+            // $table->double('min_amount',10,2)->nullable();
+            // $table->double('max_amount',10,2)->nullable();
 
-            $table->double('main_interest_rate', 10, 2)->nullable();
-            $table->double('service_interest_rate', 10, 2)->nullable();
+            // $table->integer('terms_in_month')->nullable();
+
+            // $table->double('main_interest_rate', 10, 2)->nullable();
+            // $table->double('service_interest_rate', 10, 2)->nullable();
             
             $table->string('schedule_type')->nullable(); // regular(for all schedules), scheduled            
-
-
 
             $table->boolean('is_active')->default(true);
             $table->string('remarks')->nullable();
