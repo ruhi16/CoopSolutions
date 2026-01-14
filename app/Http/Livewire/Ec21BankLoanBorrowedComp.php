d00d87ade5ff27fc517056c866205253c2360086
Hari Narayan Das Pihu16 <hndas2016@gmail.com>
2026-01-14 09:00:18 +0530

    Desk: Regular Update

---
+++ app/Http/Livewire/Ec21BankLoanBorrowedComp.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Ec21BankLoanBorrowedComp extends Component
+{
+    public function render()
+    {
+        return view('livewire.ec21-bank-loan-borrowed-comp');
+    }
+}
